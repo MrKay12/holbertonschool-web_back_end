62810d38d89cb5d3fa98a2717a5f1ebac9f0dfe2
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-13 20:11:19 +0100

    fix: disable eslint for multiple files in ES6_basic directory

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
   const result = [];
   for (const value of array) {
--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,8 +1,9 @@
+/* eslint-disable */
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments(employees) {
-        return Object.keys(employees).length;
-      },
-    };
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
 }
