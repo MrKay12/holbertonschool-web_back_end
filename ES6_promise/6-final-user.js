fa2e951d476bb6e7cd65a28941ffdab0151cb5f7
MrKay12 <quentin.lepoutre12@gmail.com>
2024-11-21 14:07:53 +0100

    fix: update import statements to remove file extensions for consistency

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 /*eslint-disable*/
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const signUpPromise = signUpUser(firstName, lastName);
