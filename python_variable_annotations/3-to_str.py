47b1ddc6b9991ca7517c7dbe0eb3dfa40ca0ae6f
MrKay12 <quentin.lepoutre12@gmail.com>
2024-12-03 17:59:36 +0100

    feat: add type annotations and docstrings to concat, to_str, and define_variables functions

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
+"""Type-annotated function to_str that takes a float n as argument"""
+
+
 def to_str(n: float) -> str:
+    """Return the string representation of the float n"""
     return str(n)
