8d9736135853d9a7a54e6b054f9f3b9020b81734
MrKay12 <quentin.lepoutre12@gmail.com>
2024-12-05 10:28:45 +0100

    feat: update wait_n function to use asyncio.as_completed for improved delay handling

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,12 @@
 
 import asyncio
 from typing import List
-from 0-basic_async_syntax import wait_random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Return the list of all the delays"""
-    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
-    return sorted(delays)
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
