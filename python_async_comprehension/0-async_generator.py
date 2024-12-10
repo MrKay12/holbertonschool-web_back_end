2f82e81939520991955186dcd7131c6085ed1ae5
MrKay12 <quentin.lepoutre12@gmail.com>
2024-12-10 12:10:54 +0100

    feat: add type hint for async_generator function to specify return type as Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """Yield a random number between 0 and 10 10 times"""
     for _ in range(10):
         await asyncio.sleep(1)
